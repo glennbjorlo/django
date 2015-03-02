f41a4918e03a0641c728887b1ea5fae90485569a
glennbjorlo <g-amb@hotmail.com>
2015-03-02 17:52:34 +0100

    Adding admin page, and customizing

--- obligen/polls/admin.py
+++ obligen/polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from polls.models import Choice, Question
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
+	]
+
+admin.site.register(Question)
+admin.site.register(Choice)
